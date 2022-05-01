2f44dfa5c0879014b1aa509e237bc8efab15214d
nejdbedoui <nejdbedoui@gmail.com>
2022-05-01 19:52:05 +0100

    open action

--- src/app/model/ActionMarketing.ts
+++ src/app/model/ActionMarketing.ts
@@ -1,18 +1,19 @@
 export class ActionMarketing{
-      idActionMarketing:String;
-      idPartenaire:String;
-      idCategorie:String;
+      idActionMarketing:string;
+      idPartenaire:string;
+      idCategorie:string;
       idPopulationCible:String;
-      libelleCanalDiffusion:String;//tv mobile sms
-      typeContenue:String; //image video
-      urlContenue:String[];
-      url:String;
-      description:String;
-      smsBody:String;
+      titre:string;
+      libelleCanalDiffusion:string;//tv mobile sms
+      typeContenue:string; //image video
+      urlContenue:string[];
+      url:string;
+      description:string;
+      smsBody:string;
       dateDebut:Date;
       dateFin:Date;
       frequence:number;
-      typeAffichageMobile:String;//pop up banniere notification
-      statut:String;//crée ,accepter ,refuser ,en cours diffusion
+      typeAffichageMobile:string;//pop up banniere notification
+      statut:string;//crée ,accepter ,refuser ,en cours diffusion
       dateCreation:Date;
 }
